cf42d857f54c312560864c909102116d0772cab9
shanmugaraj123 <shanmugaraj2003@gmail.com>
2023-10-26 23:27:34 +0530

    added some changes in styling

--- src/App.js
+++ src/App.js
@@ -8,40 +8,62 @@ function App() {
     phone: "",
     mail: "",
     language: [],
-    date: [],
+    date: "",
+    gender: "",
   });
 
   const handleChange = (event) => {
-    event.preventDefault();
-    setdata(() => ({
-      ...data,
-      [event.target.name]: event.target.value,
-    }));
+    const { type, value, checked } = event.target;
+
+    setdata((prevData) => {
+      if (type === "checkbox") {
+        if (checked) {
+          return {
+            ...prevData,
+            language: [...prevData.language, value],
+          };
+        } else {
+          return {
+            ...prevData,
+            language: prevData.language.filter((lang) => lang !== value),
+          };
+        }
+      } else {
+        return {
+          ...prevData,
+          [event.target.name]: event.target.value,
+        };
+      }
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(data);
+    console.table(data);
   };
 
   return (
     <div className="App">
       <form className="form" onSubmit={handleSubmit}>
-        <div>
-          <label>First Name</label>
-          <input
-            type="text"
-            name="firstname"
-            onChange={handleChange}
-            value={data.firstname}
-          ></input>
-          <label>Second Name</label>
-          <input
-            type="text"
-            name="secondname"
-            onChange={handleChange}
-            value={data.secondname}
-          ></input>
+        <div className="form-row">
+          <div className="form-con">
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstname"
+              onChange={handleChange}
+              value={data.firstname}
+            ></input>{" "}
+          </div>
+          <div className="form-con">
+            <label>Second Name</label>
+            <input
+              type="text"
+              name="secondname"
+              onChange={handleChange}
+              value={data.secondname}
+            ></input>
+          </div>
         </div>
         <div>
           <label>Phone Number</label>
@@ -50,33 +72,58 @@ function App() {
             name="phone"
             onChange={handleChange}
             value={data.phone}
-          ></input>
+          ></input>{" "}
           <label>Mail</label>
           <input type="email" name="mail" onChange={handleChange}></input>
         </div>
 
-        <div>
+        <div className="gender-sec">
           <label>Gender</label>
-          <input name="male" type="radio" onChange={handleChange}></input>
-          <label>Male</label>
-
-          <input name="male" type="radio" onChange={handleChange}></input>
-          <label>Female</label>
+          <div className="gender-con">
+            <input
+              name="gender"
+              value="male"
+              type="radio"
+              onChange={handleChange}
+            ></input>{" "}
+            <label>Male</label>
+            <input
+              name="gender"
+              value="Female"
+              type="radio"
+              onChange={handleChange}
+            ></input>
+            <label>Female</label>
+          </div>
         </div>
-        <div>
+        <div className="language-sec">
           <label>Language</label>
-          <input name="english" type="checkbox" onChange={handleChange}></input>
-          <label>English</label>
+          <div className="language-con">
+            <div>
+              <input
+                name="language"
+                value="english"
+                type="checkbox"
+                onChange={handleChange}
+              ></input>
+              <label>English</label>
 
-          <input name="tamil" type="checkbox" onChange={handleChange}></input>
-          <label>Tamil</label>
+              <input
+                name="language"
+                value="tamil"
+                type="checkbox"
+                onChange={handleChange}
+              ></input>
+              <label>Tamil</label>
+            </div>
+          </div>
         </div>
         <div>
           <label>Date of Birth</label>
           <input type="date" name="date" onChange={handleChange}></input>
         </div>
         <div>
-          <input type="submit" value="Send"></input>
+          <input className="form-btn" type="submit" value="Send"></input>
         </div>
       </form>
     </div>
